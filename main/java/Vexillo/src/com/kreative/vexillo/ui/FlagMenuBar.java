25c457c7497baae554d0291658ca448c6f4bddae
Rebecca G. Bettencourt <beckiejo@gmail.com>
2019-05-15 23:37:45 -0700

    Add B and E aspect ratios

--- main/java/Vexillo/src/com/kreative/vexillo/ui/FlagMenuBar.java
+++ main/java/Vexillo/src/com/kreative/vexillo/ui/FlagMenuBar.java
@@ -33,8 +33,10 @@ public class FlagMenuBar extends JMenuBar {
 		view.add(new SetHoistMenuItem("Large", KeyEvent.VK_L, KeyEvent.VK_3, frame, 360));
 		view.addSeparator();
 		view.add(new SetAspectRatioMenuItem("Automatic", KeyEvent.VK_A, KeyEvent.VK_A, frame, 0, 0));
+		view.add(new SetAspectRatioMenuItem("5:3", KeyEvent.VK_5, KeyEvent.VK_B, frame, 5, 3));
 		view.add(new SetAspectRatioMenuItem("3:2", KeyEvent.VK_3, KeyEvent.VK_C, frame, 3, 2));
 		view.add(new SetAspectRatioMenuItem("4:3", KeyEvent.VK_4, KeyEvent.VK_D, frame, 4, 3));
+		view.add(new SetAspectRatioMenuItem("5:4", KeyEvent.VK_UNDEFINED, KeyEvent.VK_E, frame, 5, 4));
 		view.add(new SetAspectRatioMenuItem("1:1", KeyEvent.VK_1, KeyEvent.VK_F, frame, 1, 1));
 		view.add(new SetAspectRatioMenuItem("2:1", KeyEvent.VK_2, KeyEvent.VK_H, frame, 2, 1));
 		view.add(new SetAspectRatioMenuItem("16:11", KeyEvent.VK_6, KeyEvent.VK_K, frame, 16, 11));
